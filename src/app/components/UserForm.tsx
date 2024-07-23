f97d487ac808a08f30bf20358d38b1197fb309ee
Oscar Herrera <a.oscarherrera@gmail.com>
2024-07-23 14:38:03 -0500

    user can set their personalize diet

--- src/app/components/UserForm.tsx
+++ src/app/components/UserForm.tsx
@@ -1,18 +1,43 @@
 "use client";
 import { useState } from "react";
 export default function UserForm() {
-    const [UserData, setUserData] = useState({weight: '', height: '', bodyfat: '', age: ''})
+    const [userData, setUserData] = useState({calories: 0, protein: 0, carbohydrates: 0, fats: 0})
+    const [diet, setDiet] = useState({dietCals: 0, proGrams: 0, carbGrams: 0, fatGrams: 0})
 
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const {name, value} = e.target;
+      setUserData(prev => {
+        const updatedUserData = {...prev, [name]: Number(value)};
+        handleDietNutrients(updatedUserData);
+        return updatedUserData
+      });
+    };
 
+    const getGramsFromPercent = (userData: { calories: number, protein: number, carbohydrates: number, fats: number }) => {
+      const diet = userData.calories;
+      const proCals = (userData.protein / 100) * diet;
+      const carbCals = (userData.carbohydrates / 100) * diet;
+      const fatCals = (userData.fats / 100) * diet;
+      let carbsInGrams = Math.ceil(carbCals / 4);
+      let proInGrams = Math.ceil(proCals / 4);
+      let fatInGrams = Math.ceil(fatCals / 9);
+      return {carbsInGrams, proInGrams, fatInGrams, diet}
+    }
 
+    const handleDietNutrients = (userData: { calories: number, protein: number, carbohydrates: number, fats: number }) => {
+      const nutrients = getGramsFromPercent(userData);
+      console.log(nutrients)
+      const {carbsInGrams, proInGrams, fatInGrams, diet} = nutrients;
+      setDiet(prev => ({...prev, dietCals: diet, proGrams: proInGrams, carbGrams: carbsInGrams, fatGrams: fatInGrams}))
+    }
+    
 
   return (
     <div className="flex h-screen w-full flex-col bg-zinc-800">
       <h1 className="m-auto w-8/12 text-2xl  text-white md:text-5xl lg:w-7/12">Your Details</h1>
       <p className="mx-auto w-8/12 text-white md:text-2xl lg:w-7/12">
-        Enter your details and select your diet goal so we can whip up a diet
-        plan
+       Personlize your own diet
       </p>
       <form
         className="m-auto flex h-[50vh] w-8/12 flex-col items-center justify-center gap-1 md:gap-3 rounded-lg bg-green-200 lg:h-[60vh] lg:w-5/12"
@@ -20,54 +45,60 @@ export default function UserForm() {
         method="POST"
       >
         <div className="flex w-9/12 flex-col ">
-          <label htmlFor="weight">Weight</label>
+          <label htmlFor="calories">Calories</label>
           <input
             className="rounded border border-black md:h-11 lg:w-9/12"
             type="text"
-            name="weight"
-            id="weight"
+            name="calories"
+            id="calories"
+            onChange={handleChange}
+            value={userData.calories}
           />
         </div>
         <div className="flex w-9/12 flex-col ">
-          <label htmlFor="height">Height</label>
+          <label htmlFor="height">{`${userData.protein}% Protein`}</label>
+          <p>{diet.proGrams + ' grams'}</p>
           <input
             className="rounded border border-black md:h-12 lg:w-9/12"
-            type="text"
-            name="height"
-            id="height"
+            type="range"
+            name="protein"
+            id="protien"
+            step="5"
+            onChange={handleChange}
+            value={userData.protein}
           />
         </div>
         <div className="flex w-9/12 flex-col ">
-          <label htmlFor="body-fat">Body fat %</label>
+          <label htmlFor="carbohydrates">{`${userData.carbohydrates}% Carbs`}</label>
+          <p>{diet.carbGrams + ' grams'}</p>
           <input
             className="rounded border border-black md:h-12 lg:w-9/12"
-            type="text"
-            name="body-fat"
-            id="body-fat"
+            type="range"
+            name="carbohydrates"
+            id="carbohydrates"
+            step={5}
+            onChange={handleChange}
+            value={userData.carbohydrates}
           />
         </div>
         <div className="flex w-9/12 flex-col ">
-          <label htmlFor="age">Age</label>
+          <label htmlFor="Fats">{`${userData.fats}% Fats`}</label>
+          <p>{diet.fatGrams + ' grams'}</p>
           <input
             className="rounded border border-black md:h-12 lg:w-9/12"
-            type="text"
-            name="age"
-            id="age"
+            type="range"
+            name="fats"
+            id="fats"
+            step={5}
+            onChange={handleChange}
+            value={userData.fats}
           />
-        </div>
-        <div>
-          <label htmlFor="diet-type">
-            Diet type:
-            <select name="selected diet" id="diet-type">
-              <option value="gain muscle">Gain muscle</option>
-              <option value="lose weight">Lose weight</option>
-            </select>
-          </label>
         </div>
         <button onClick={(e) => {
           e.preventDefault()
         }} className="bg-white rounded-md w-36">Enter</button>
-      </form>
+      </form> 
     </div>
+    
   );
 }
