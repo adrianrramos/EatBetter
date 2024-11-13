2c082c67305e51c57603ecbd32c12e869a7888d6
Oscar Herrera <a.oscarherrera@gmail.com>
2024-11-13 08:55:34 -0600

    setup auth

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -22,7 +22,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/user')
+  redirect('/dashboard')
 }
 
 export async function signup(formData: FormData) {
@@ -38,6 +38,7 @@ export async function signup(formData: FormData) {
   const { error } = await supabase.auth.signUp(data)
 
   if (error) {
+    console.log(error)
     redirect('/error')
   }
 
