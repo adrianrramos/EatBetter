d2bc839c4141c0a5dab6ec181b5b5023edaf92b4
Oscar Herrera <a.oscarherrera@gmail.com>
2024-11-13 09:11:58 -0600

    added dashboard ui to landing page

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar"
 import Image from "next/image"
-import AppIcon from "../../../assets/App-mockup.png"
 import Link from "next/link"
 
 
@@ -9,12 +8,13 @@ export default function Hero () {
         <main className="bg-zinc-900 h-full pb-12 md:pb-20 text-white pt-4 ">
             <Navbar/>
             <Image
-                src={AppIcon}
+                src={'https://acrsqpvtjvymrxgvsmuf.supabase.co/storage/v1/object/sign/assets/images/hero-product.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhc3NldHMvaW1hZ2VzL2hlcm8tcHJvZHVjdC5wbmciLCJpYXQiOjE3MzA5ODk1OTQsImV4cCI6MTc2MjUyNTU5NH0.6kmGTikLvCkGuMNM5BJq8-kPk5r1o4ODdYK_F-IuM4s&t=2024-11-07T14%3A26%3A34.738Z'}
                 height={500}
                 width={500}
                 alt="App icon"
                 priority
                 className="m-auto py-16 lg:py-12"
+                quality={80}
             />
             <h1 className="font-bold text-2xl m-auto text-center pb-4 md:text-4xl lg:text-6xl"><span className="text-green-300">Eat Better</span>, a calories <br/> tracking app that coaches you</h1>
             <p className="text-xs w-64 m-auto text-center md:text-base md:w-96">Focus on day to day training. Eat better handles the stress of diet planning to make your fitness goals a reality</p>
