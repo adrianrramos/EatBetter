f8583ab1e1d5190d29f9544c33db39267a7784d6
Oscar Herrera <a.oscarherrera@gmail.com>
2024-11-13 09:13:27 -0600

    rendered macro goal per meal

--- src/app/components/MacroMeter.tsx
+++ src/app/components/MacroMeter.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
+
 
 interface MacroMeterProps {
-    initial: string;
-    macroType: number;
+    macroType: string;
+    initialMacro: number;
+    dailyMacroGoal: number;
     bgColor: string;
 }
 
-export default function MacroMeter ({initial, macroType, bgColor}: MacroMeterProps) {
-    const [fill, setFill] = useState({style: ''})
 
-    const handleFill = () => {
-        
-    }
+
+export default function MacroMeter ({macroType, initialMacro, dailyMacroGoal, bgColor}: MacroMeterProps) {
+   
+
+
+
+   
+
+
+
 
    return (
     <div className={`flex text-sm justify-center items-center border ${bgColor} w-1/3 rounded`}>
-
-        {macroType}/25{initial}
+        {initialMacro}/
+        {dailyMacroGoal}{macroType}
     </div>
    )
 }
