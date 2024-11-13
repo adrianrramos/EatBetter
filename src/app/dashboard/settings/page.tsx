9477f79fa90c816746fb0923dd7ab2244218bd91
Oscar Herrera <a.oscarherrera@gmail.com>
2024-11-13 09:21:28 -0600

    new dashboard & settings page

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,16 @@
+import BottomTabs from "@/app/components/BottomTabs"
+import DietCalculator from "@/app/components/DietCalculator"
+
+
+
+
+function SettingsPage() {
+  return (
+    <>
+      <DietCalculator/>
+      <BottomTabs/>
+    </>
+  )
+}
+
+export default SettingsPage
